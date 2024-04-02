9650e5639ede49ca82b72a20f6ea05079bc17dfe
Felipe Daniel <felipe.daniel.henriquez@gmail.com>
2024-04-02 14:14:52 -0300

    corregido error de tipeo

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("hola mundo");
+const http = require('http');
+
+const PORT = 8080
+
+const server = http.createServer((req, res) => {
+    console.log(req);
+
+    res.setHeader('Content-Disposition', 'attachment; filename=lista.csv')
+    res.writeHead(200, {
+        'Content-Type': 'application/csv'
+    });
+
+    /* const persona = {
+           id: 1,
+           nombre: 'Felipe',
+       } */
+    // res.write( JSON.stringify(persona) );
+
+    res.write('id, nombre\n')
+    res.write('1, Fernando\n')
+    res.write('2, Maria\n')
+    res.write('3, Juan\n')
+    res.write('4, Pedro\n')
+    res.end();
+})
+
+server.listen(PORT);
+
+console.log("Escuchando en el puerto: ", PORT);
